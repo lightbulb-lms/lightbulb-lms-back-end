15b1e804e1c0b122dbb9a49965b07687f76614cb
Christopher Nellis <christopher.t.nellis@gmail.com>
2020-10-26 21:45:12 -0400

    Initial commit

---
+++ src/test/java/edu/uncc/itcs/lightbulblms/LightbulbLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uncc.itcs.lightbulblms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightbulbLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

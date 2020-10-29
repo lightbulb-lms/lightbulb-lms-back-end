1ac6f4654b1524cd5ff8a339cab02c1ba450c48a
Christopher Nellis <christopher.t.nellis@gmail.com>
2020-10-29 00:15:06 -0400

    Add security to Swagger

--- src/main/java/edu/uncc/itcs/lightbulblms/config/SwaggerConfig.java
+++ src/main/java/edu/uncc/itcs/lightbulblms/config/SwaggerConfig.java
@@ -6,38 +6,40 @@ import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.HttpAuthenticationScheme;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
+    public static final List<SecurityContext> AUTHENTICATED_API = Collections.singletonList(SecurityContext.builder()
+            .securityReferences(
+                    Collections.singletonList(
+                            new SecurityReference("Authenticated API", new AuthorizationScope[0])))
+            .operationSelector(operationContext -> true)
+            .build()
+    );
+
+    public static final List<SecurityScheme> SECURITY_SCHEMES = Collections.singletonList(
+            new HttpAuthenticationScheme("Authenticated API",
+                    "Paste in Okta Access Token JWT here",
+                    "http", "bearer", "jwt", Collections.emptyList()));
+
     @Bean
     public Docket api() {
-
         return new Docket(DocumentationType.OAS_30)
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo())
-                .securitySchemes(Collections.singletonList(
-                        new HttpAuthenticationScheme("Authenticated API",
-                                "Paste in Okta Access Token JWT here",
-                                "http", "bearer", "jwt", Collections.emptyList())))
-                .securityContexts(Collections.singletonList(SecurityContext.builder()
-                        .securityReferences(
-                                Collections.singletonList(
-                                    new SecurityReference("Authenticated API", new AuthorizationScope[0])))
-                        .operationSelector(operationContext -> true)
-                        .build()
-                ))
+                .securitySchemes(SECURITY_SCHEMES)
+                .securityContexts(AUTHENTICATED_API)
                 ;
     }
 
